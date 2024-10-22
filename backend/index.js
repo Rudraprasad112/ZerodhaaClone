c884cbe84c2ed8c4a84519173b40472a957edfe9
Rudra panchanan bhuyan <rudrabhuyan6@gmail.com>
2024-10-22 11:27:47 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express  from 'express'
 
 import mongoose from 'mongoose'
@@ -7,13 +9,14 @@ import  Routes  from './Routers.js'
 import cors from "cors"
 import bodyParser from "body-parser";
 const port = process.env.PORT || 8080;
+const url = process.env.MONGO_URL;
 
 app.use(bodyParser.json())
 app.use(cors());
 app.use("/entry",Routes);
 const connectionDb = async()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/Zerodha")
+        await mongoose.connect(url)
         console.log("connect to db");
         
     } catch (error) {
